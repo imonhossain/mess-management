f0a832efc5aca805b129e5cb96c24a0bd70ff2ce
Imon Hossain <imon523029@gmail.com>
2021-04-16 15:46:45 +0600

    expense crud done

--- MessManagement/MessManagement/src/main/java/com/imon/MessManagement/controller/ExpenseController.java
+++ MessManagement/MessManagement/src/main/java/com/imon/MessManagement/controller/ExpenseController.java
@@ -9,36 +9,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-//@RestController
-//@RequestMapping("/api/expense")
+@RestController
+@RequestMapping("/api/expense")
 public class ExpenseController {
-//    private final ExpenseService service;
-//    public ExpenseController(ExpenseService service){
-//        this.service = service;
-//    }
-//
-//    @GetMapping("")
-//    public ResponseEntity<List<Expense>> getAllExpense(){
-//        return new ResponseEntity<>(service.getAllExpense(), HttpStatus.OK);
-//    }
-//    @PostMapping("")
-//    public  ResponseEntity<Expense> saveExpense(@RequestBody Expense expense){
-//        return new ResponseEntity<>(service.saveExpense(expense), HttpStatus.CREATED);
-//    }
-//
-//    @GetMapping("/${id}")
-//    public ResponseEntity<Optional<Expense>> getExpenseById(@PathVariable Integer id){
-//        return new ResponseEntity<>(service.getExpenseById(id), HttpStatus.ACCEPTED);
-//    }
-//
-//    @PutMapping("")
-//    public  ResponseEntity<Expense> updateExpense(@RequestBody Expense expense){
-//        return new ResponseEntity<>(service.updateExpense(expense), HttpStatus.ACCEPTED);
-//    }
-//
-//    @DeleteMapping("/${id}")
-//    public void deleteExpense(@PathVariable Integer id){
-//        service.deleteExpense(id);
-//    }
+    private final ExpenseService service;
+    public ExpenseController(ExpenseService service){
+        this.service = service;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Expense>> getAllExpense(){
+        return new ResponseEntity<>(service.getAllExpense(), HttpStatus.OK);
+    }
+    @PostMapping("")
+    public  ResponseEntity<Expense> saveExpense(@RequestBody Expense expense){
+        return new ResponseEntity<>(service.saveExpense(expense), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Expense>> getExpenseById(@PathVariable Integer id){
+        return new ResponseEntity<>(service.getExpenseById(id), HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("")
+    public  ResponseEntity<Expense> updateExpense(@RequestBody Expense expense){
+        return new ResponseEntity<>(service.updateExpense(expense), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteExpense(@PathVariable Integer id){
+        service.deleteExpense(id);
+    }
 
 }
--- MessManagement/MessManagement/src/main/java/com/imon/MessManagement/service/ExpenseService.java
+++ MessManagement/MessManagement/src/main/java/com/imon/MessManagement/service/ExpenseService.java
@@ -7,23 +7,23 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-//@Service
+@Service
 public class ExpenseService {
-//    @Autowired
-//    ExpenseRepository repository;
-//    public List<Expense> getAllExpense(){
-//        return repository.findAll();
-//    }
-//    public Expense saveExpense(Expense expense){
-//        return repository.save(expense);
-//    }
-//    public Optional<Expense> getExpenseById(Integer id){
-//        return repository.findById(id);
-//    }
-//    public Expense updateExpense(Expense expense){
-//        return repository.save(expense);
-//    }
-//    public void deleteExpense(Integer id){
-//         repository.deleteById(id);
-//    }
+    @Autowired
+    ExpenseRepository repository;
+    public List<Expense> getAllExpense(){
+        return repository.findAll();
+    }
+    public Expense saveExpense(Expense expense){
+        return repository.save(expense);
+    }
+    public Optional<Expense> getExpenseById(Integer id){
+        return repository.findById(id);
+    }
+    public Expense updateExpense(Expense expense){
+        return repository.save(expense);
+    }
+    public void deleteExpense(Integer id){
+         repository.deleteById(id);
+    }
 }
